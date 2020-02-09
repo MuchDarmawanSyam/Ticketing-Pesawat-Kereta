f51c29c0e6bcdb236c004f7218f11c9b218a8945
Much Darmawan Iriansyah Syam <mchdarmawan@gmail.com>
2020-02-09 10:59:50 +0900

    Merefaktur code alert dengan memanggil dri file lain agar reusable pada file entries-manageOfficer.php

--- admin-page/App/entries-manageOfficers.php
+++ admin-page/App/entries-manageOfficers.php
@@ -186,43 +186,7 @@
   <script>
       $('#dataTable2').dataTable();
   </script>
-
-
-
-  <!-- Alert Penambahan Data Petugas -->
-  <?php 
-    if (isset($_SESSION['status_operasi'])){
-      
-      if ($_SESSION['status_operasi'] == TRUE){
-  ?>
-
-        <script>
-          Swal.fire({
-             icon: 'success',
-             title: 'Success',
-             text: '<?= $_SESSION['msg']; ?>',
-          });
-        </script>
-
-  <?php
-      }elseif ($_SESSION['status_operasi'] == FALSE){
-  ?>
-
-        <script>
-          Swal.fire({
-             icon: 'error',
-             title: 'OOps...',
-             text: '<?= $_SESSION['msg']; ?>',
-          });
-        </script>
-
-  <?php
-      }
-    }
-       // Membuat Alert Muncul Sekali
-       $_SESSION['status_operasi'] = NULL;
-  ?>
-
+  <?php include("Assembly_srcs/srcs - alerts.php"); ?>
   
 
 </body>
